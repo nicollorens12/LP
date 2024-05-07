37d4ae4e504dea0ccec456183195ec521821be02
Nico Llorens <nicollorenss@gmail.com>
2024-05-08 00:19:05 +0200

    Bug mapa de tipos vacio entre calls

--- HinNer/hmVisitor.py
+++ HinNer/hmVisitor.py
@@ -43,6 +43,7 @@ class hmVisitor(ParseTreeVisitor):
         self.atomCount = 0
         self.root_node = None
         self.type_df = pd.DataFrame(columns=['Elemento', 'Tipo'])
+        self.varTypeMap = dict()
         self.current_type = 'a'
 
 
@@ -114,51 +115,43 @@ class hmVisitor(ParseTreeVisitor):
         return FunctionNode(operator=operator)
     
     def generate_dot(self, node):
+        print("entro a generate dot, varTypeMap es", self.varTypeMap, "y el nodo es", node)
         root_node = None
         if isinstance(node, ApplicationNode):
             root_node = pydot.Node(f"apl_{str(self.aplicationCount)}", label=f"@\n{self.current_type}")
+            self.current_type = chr(ord(self.current_type)+1)
             self.aplicationCount += 1
             self.graph.add_node(root_node)
-            self.current_type = chr(ord(self.current_type) + 1)
+            
             if node.expression:
                 expression_node = self.generate_dot(node.expression)
-                if expression_node:
-                    
-                    self.graph.add_edge(pydot.Edge(root_node, expression_node))
+                self.graph.add_edge(pydot.Edge(root_node, expression_node))
             if node.atom:
-                
+                print("Voy a generar el nodo atom", node.atom)
                 atom_node = self.generate_dot(node.atom)
-
-                if atom_node:
-                    
-                    self.graph.add_edge(pydot.Edge(root_node, atom_node))
+                self.graph.add_edge(pydot.Edge(root_node, atom_node))
             
         elif isinstance(node, AbstractionNode):
             abstraction_node = pydot.Node(f"abs_{str(self.aplicationCount)}" ,label=f"ʎ\n{self.current_type}" )
+            self.current_type = chr(ord(self.current_type)+1)
             self.abstractionCount += 1
             self.graph.add_node(abstraction_node)
-            
-            # Add node for variable
+
             if node.variable:
-                self.current_type =  chr(ord(self.current_type) + 1)
                 variable_node = pydot.Node(f"var_{str(self.atomCount)}",label=f"{node.variable}\n{self.current_type}")
+                self.current_type = chr(ord(self.current_type)+1)
                 self.atomCount += 1
                 self.graph.add_node(variable_node)
                 self.graph.add_edge(pydot.Edge(abstraction_node, variable_node))
-
-            expression_node = self.generate_dot(node.expression)
-            if expression_node:
-                self.current_type =  chr(ord(self.current_type) + 1)
-                # Create a new node for each abstraction
+            if node.expression:
+                expression_node = self.generate_dot(node.expression)
                 self.graph.add_node(expression_node)
                 self.graph.add_edge(pydot.Edge(abstraction_node, expression_node))
-                
-                
             root_node = abstraction_node
+
         elif isinstance(node, FunctionNode):
             function_node = pydot.Node(f"func_{str(self.functionCount)}",label=f"({node.operator})\n(N->(N->N))")
             self.functionCount += 1
-            
             self.graph.add_node(function_node)
             root_node = function_node
 
@@ -166,8 +159,18 @@ class hmVisitor(ParseTreeVisitor):
             if isinstance(node.value, int):
                 atom_node = pydot.Node(f"atom_{str(self.atomCount)}",label=f"{node.value}\nN")
             else:
-                atom_node = pydot.Node(f"atom_{str(self.atomCount)}",label=f"{node.value}\n{self.current_type}")
-            
+                print("Tengo un node value", node.value, "y el mapa de tipos es", self.varTypeMap)
+                if node.value in self.varTypeMap:
+                    print("Encontre el tipo en el mapa de tipos")
+                    node_type = self.varTypeMap[node.value]
+                    atom_node = pydot.Node(f"atom_{str(self.atomCount)}",label=f"{node.value}\n{node_type}")
+                else:
+                    print("No encontre el tipo en el mapa de tipos")
+                    self.varTypeMap[node.value] = self.current_type
+                    print("Ahora el mapa de tipos es", self.varTypeMap)
+                    atom_node = pydot.Node(f"atom_{str(self.atomCount)}",label=f"{node.value}\n{self.current_type}")
+                    self.current_type = chr(ord(self.current_type)+1)
+                
             self.atomCount += 1
             self.graph.add_node(atom_node)
             root_node = atom_node
